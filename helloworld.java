aa9d18fd0d428ca9cc9b8d105dcab9bd99bc4d62
Madhuvakitikada1 <94283548+Madhuvakitikada1@users.noreply.github.com>
2021-11-23 13:40:54 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -5,12 +5,12 @@ public class HelloWorld {
         // Creates a reader instance which takes
         // input from standard input - keyboard
         Scanner reader = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print(10);
 
         // nextInt() reads the next integer from the keyboard
         int number = reader.nextInt();
 
         // println() prints the following line to the output screen
-        System.out.println("You entered: " + number);
+        System.out.println(10 + number);
     }
 }
